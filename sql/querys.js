3a5d607da29805c7a13fc15dc20690b1c2e86fdd
axelme28 <a.mendez.m@edu.utc.mx>
2021-10-12 19:37:23 -0500

    add service for logIn

--- sql/querys.js
+++ sql/querys.js
@@ -5,3 +5,6 @@ exports.insertDocumental = 'call documentales.insertar_documental(:Nombre,:Fecha
 exports.deleteDocumental = 'call documentales.Eliminar_documental(:idDocumental);';
 
 exports.getDocumetal = 'call documentales.ver_documental(:idDocumental);';
+
+//TODO: add query for logIn, it must be an procedure
+exports.logIn = 'select 1+1';
