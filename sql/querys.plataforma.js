e572513800e059a450e395bc8083c366776b4309
Axel Mendez <71779664+axelme28@users.noreply.github.com>
2021-11-03 07:09:33 -0600

    add querys for pricedures

---
+++ sql/querys.plataforma.js
@@ -0,0 +1,4 @@
+exports.queryCreateUser =  'call plataforma_educativa.`Crear Usuario`(:email,:password,:idRol,:idUniversidad);'
+
+exports.queryCreateAlumno =  'call plataforma_educativa.`Crear Alumno`( nombre,:apellidoPaterno,:apellidoMaterno,:boleta,:idUsuario,:idEquipo);'
+
