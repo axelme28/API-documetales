2be2207e0d40a176e1272b7d50e3f4f61dce8f9c
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-11-08 23:07:25 -0600

    FIX: ajuste en controllers y regreso de data en estructura de datos confusa por sequelize

--- sql/querys.plataforma.js
+++ sql/querys.plataforma.js
@@ -5,7 +5,7 @@ exports.queryCreateAlumno =
   "insert into plataforma_educativa.alumno(nom_alumno,apellido_paterno,idUsu,boleta,apellido_materno)values(:nombre,:apellidoPaterno,:idUsuario,:boleta,:apellidoMaterno);";
 
 exports.queryCreateUniversidad =
-  "call plataforma_educativa.`Crear Universidad`(:nombre,:codigo);";
+  "call plataforma_educativa.`Crear Universidad`(:nombre,:convenio);";
 
 exports.queryCreateProfesor =
   "insert into plataforma_educativa.docente(id, nom_docente,apellido_materno, id_usu, no_trabajador, fecha_naci,apellido_paterno) values(default,:nombre,:apellidoMaterno,:idUsuario,:noTrabajador,:fechaNacimiento,:apellidoPaterno);";
@@ -13,8 +13,10 @@ exports.queryCreateProfesor =
 exports.queryCreateAdmin =
   "call plataforma_educativa.`Crear Administrador`(:nom_admi, :apellido_paterno,:apellido_materno, :idUsuario, :direccion, :telefono);";
 
-exports.queryCreateEquipo = 
-'insert into equipo(id,nom_equipo,codigo_equipo) values (default,:nombre,:codigo);';
+exports.queryCreateEquipo =
+  "insert into equipo(id,nom_equipo,codigo_equipo) values (default,:nombre,:codigo);";
 
-exports.queryCreateGrupo = 
-'insert into grupo(id,equipo_id,usuario_id) values(default,:idEquipo,:idDocente)';
+exports.queryCreateGrupo =
+  "insert into grupo(id,equipo_id,usuario_id) values(default,:idEquipo,:idDocente)";
+
+exports.queryExistConvenio = `SELECT convenio FROM universidad`;
